450bc76da9317b4704a966592a8ef00a714d8697
c17julka <37792321+c17julka@users.noreply.github.com>
2020-04-17 18:02:28 +0200

    All of it - Results

---
+++ Results/result.js
@@ -0,0 +1,555 @@
+var json = [
+    {
+      "rt": 26063,
+      "responses": "",
+      "question_order": "",
+      "trial_type": "instructions",
+      "trial_index": 0,
+      "time_elapsed": 26076,
+      "internal_node_id": "0.0-0.0",
+      "subjectId": "l8j3k5vhy2wbhvft71g2"
+    },
+    {
+      "rt": 18150,
+      "responses": {
+        "internet": 5,
+        "news": 3
+      },
+      "question_order": [
+        0,
+        1
+      ],
+      "trial_type": "survey-likert",
+      "trial_index": 1,
+      "time_elapsed": 44250,
+      "internal_node_id": "0.0-1.0",
+      "subjectId": "l8j3k5vhy2wbhvft71g2"
+    },
+    {
+      "rt": 52908,
+      "responses": {
+        "appearance": 3,
+        "readability": 6,
+        "readability2": 3,
+        "future": 4
+      },
+      "question_order": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "trial_type": "survey-likert",
+      "trial_index": 2,
+      "time_elapsed": 97176,
+      "internal_node_id": "0.0-2.0-0.0",
+      "subjectId": "l8j3k5vhy2wbhvft71g2"
+    },
+    {
+      "rt": 32009,
+      "responses": {
+        "appearance": 1,
+        "readability": 3,
+        "readability2": 0,
+        "future": 0
+      },
+      "question_order": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "trial_type": "survey-likert",
+      "trial_index": 3,
+      "time_elapsed": 129209,
+      "internal_node_id": "0.0-2.0-1.0",
+      "subjectId": "l8j3k5vhy2wbhvft71g2"
+    },
+    {
+      "rt": 39666,
+      "responses": {
+        "appearance": 0,
+        "readability": 2,
+        "readability2": 0,
+        "future": 0
+      },
+      "question_order": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "trial_type": "survey-likert",
+      "trial_index": 4,
+      "time_elapsed": 168891,
+      "internal_node_id": "0.0-2.0-2.0",
+      "subjectId": "l8j3k5vhy2wbhvft71g2"
+    },
+    {
+      "rt": 1626,
+      "responses": "",
+      "question_order": "",
+      "trial_type": "html-button-response",
+      "trial_index": 5,
+      "time_elapsed": 170529,
+      "internal_node_id": "0.0-3.0",
+      "subjectId": "l8j3k5vhy2wbhvft71g2"
+    },
+    {
+      "rt": 4655,
+      "responses": "",
+      "question_order": "",
+      "trial_type": "instructions",
+      "trial_index": 0,
+      "time_elapsed": 4658,
+      "internal_node_id": "0.0-0.0",
+      "subjectId": "tycefhbu0d0olcamung6"
+    },
+    {
+      "rt": 14116,
+      "responses": {
+        "internet": 6,
+        "news": 5
+      },
+      "question_order": [
+        0,
+        1
+      ],
+      "trial_type": "survey-likert",
+      "trial_index": 1,
+      "time_elapsed": 18781,
+      "internal_node_id": "0.0-1.0",
+      "subjectId": "tycefhbu0d0olcamung6"
+    },
+    {
+      "rt": 32195,
+      "responses": {
+        "appearance": 4,
+        "readability": 3,
+        "readability2": 5,
+        "future": 3
+      },
+      "question_order": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "trial_type": "survey-likert",
+      "trial_index": 2,
+      "time_elapsed": 50981,
+      "internal_node_id": "0.0-2.0-0.0",
+      "subjectId": "tycefhbu0d0olcamung6"
+    },
+    {
+      "rt": 21862,
+      "responses": {
+        "appearance": 2,
+        "readability": 3,
+        "readability2": 4,
+        "future": 2
+      },
+      "question_order": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "trial_type": "survey-likert",
+      "trial_index": 3,
+      "time_elapsed": 72848,
+      "internal_node_id": "0.0-2.0-1.0",
+      "subjectId": "tycefhbu0d0olcamung6"
+    },
+    {
+      "rt": 13313,
+      "responses": {
+        "appearance": 1,
+        "readability": 0,
+        "readability2": 2,
+        "future": 0
+      },
+      "question_order": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "trial_type": "survey-likert",
+      "trial_index": 4,
+      "time_elapsed": 86166,
+      "internal_node_id": "0.0-2.0-2.0",
+      "subjectId": "tycefhbu0d0olcamung6"
+    },
+    {
+      "rt": 1451,
+      "responses": "",
+      "question_order": "",
+      "trial_type": "html-button-response",
+      "trial_index": 5,
+      "time_elapsed": 87622,
+      "internal_node_id": "0.0-3.0",
+      "subjectId": "tycefhbu0d0olcamung6"
+    },
+    {
+      "rt": 83405,
+      "responses": "",
+      "question_order": "",
+      "trial_type": "instructions",
+      "trial_index": 0,
+      "time_elapsed": 83406,
+      "internal_node_id": "0.0-0.0",
+      "subjectId": "hsbm7uury8prebyom70x"
+    },
+    {
+      "rt": 27354,
+      "responses": {
+        "internet": 6,
+        "news": 5
+      },
+      "question_order": [
+        0,
+        1
+      ],
+      "trial_type": "survey-likert",
+      "trial_index": 1,
+      "time_elapsed": 110763,
+      "internal_node_id": "0.0-1.0",
+      "subjectId": "hsbm7uury8prebyom70x"
+    },
+    {
+      "rt": 100184,
+      "responses": {
+        "appearance": 3,
+        "readability": 4,
+        "readability2": 2,
+        "future": 1
+      },
+      "question_order": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "trial_type": "survey-likert",
+      "trial_index": 2,
+      "time_elapsed": 210952,
+      "internal_node_id": "0.0-2.0-0.0",
+      "subjectId": "hsbm7uury8prebyom70x"
+    },
+    {
+      "rt": 39419,
+      "responses": {
+        "appearance": 2,
+        "readability": 3,
+        "readability2": 2,
+        "future": 2
+      },
+      "question_order": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "trial_type": "survey-likert",
+      "trial_index": 3,
+      "time_elapsed": 250375,
+      "internal_node_id": "0.0-2.0-1.0",
+      "subjectId": "hsbm7uury8prebyom70x"
+    },
+    {
+      "rt": 65394,
+      "responses": {
+        "appearance": 2,
+        "readability": 3,
+        "readability2": 2,
+        "future": 2
+      },
+      "question_order": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "trial_type": "survey-likert",
+      "trial_index": 4,
+      "time_elapsed": 315775,
+      "internal_node_id": "0.0-2.0-2.0",
+      "subjectId": "hsbm7uury8prebyom70x"
+    },
+    {
+      "rt": 1772,
+      "responses": "",
+      "question_order": "",
+      "trial_type": "html-button-response",
+      "trial_index": 5,
+      "time_elapsed": 317551,
+      "internal_node_id": "0.0-3.0",
+      "subjectId": "hsbm7uury8prebyom70x"
+    }
+  ]
+
+// Variables for data
+
+var question_1 = [];
+var question_2 = [];
+var question_3 = [];
+var question_4 = [];
+
+// Mean times general
+
+for (u=1;u<5;u++)
+{
+    for (i=0;i<json.length;i++) 
+    {
+        if(json[i].trial_index == u)
+        {
+            this["question_"+u].push(json[i].rt);
+            //trial.push(json[i].rt);
+        }
+        
+    }
+}
+
+for (x=1;x<5;x++)
+{
+    var divide = question_1.length + 1;
+    this["total_"+x] = this["question_"+x].reduce((a, b) => a + b, 0);
+    this["mt_"+x] = this["total_"+x] / divide;
+}
+
+// Mean times high / low internet / news usage
+
+var question_1_highInt = [];
+var question_2_highInt = [];
+var question_3_highInt = [];
+var question_4_highInt = [];
+
+var question_1_lowInt = [];
+var question_2_lowInt = [];
+var question_3_lowInt = [];
+var question_4_lowInt = [];
+
+var question_1_highNews = [];
+var question_2_highNews = [];
+var question_3_highNews = [];
+var question_4_highNews = [];
+
+var question_1_lowNews = [];
+var question_2_lowNews = [];
+var question_3_lowNews = [];
+var question_4_lowNews = [];
+
+// for (y=1;y<5;y++)
+// {
+//     for (i=0;i<json.length;i++) 
+//     {
+//         if(json[i].trial_index == 1 && (json[1]).responses.internet > 3)
+//         {
+//             this["question_"+u+"highInt"].push(json[i].rt);
+            
+//         }
+        
+//     }
+// }
+
+
+
+// for (x=1;x<5;x++)
+// {
+//     var divide = question_1.length + 1;
+//     this["total_"+x] = this["question_"+x].reduce((a, b) => a + b, 0);
+//     this["mt_"+x] = this["total_"+x] / divide;
+// }
+
+// Charts
+
+// var ctx1 = document.getElementById('totalChart').getContext('2d');
+var ctx2 = document.getElementById('meanChart').getContext('2d');
+
+// var totalTime = new Chart(ctx1, {
+//     type: 'bar',
+//     data: {
+//         labels: ['No AH', 'AH Hiding', 'AH Hiding + Annotation'],
+//         datasets: [{
+//             label: 'Total MS Spent',
+//             data: [total_2, total_3, total_4],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero: true,
+//                     suggestedMax: 200000
+//                 }
+//             }]
+//         }
+//     }
+// });
+
+var meanTime = new Chart(ctx2, {
+    type: 'bar',
+    data: {
+        labels: ['No AH', 'AH Hiding', 'AH Hiding + Annotation'],
+        datasets: [{
+            label: 'MT MS Spent',
+            data: [mt_2, mt_3, mt_4],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var meanTimeHighInternet = new Chart(ctx2, {
+    type: 'bar',
+    data: {
+        labels: ['No AH', 'AH Hiding', 'AH Hiding + Annotation'],
+        datasets: [{
+            label: 'MT MS Spent',
+            data: [mt_2, mt_3, mt_4],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var meanTimeHighNews = new Chart(ctx2, {
+    type: 'bar',
+    data: {
+        labels: ['No AH', 'AH Hiding', 'AH Hiding + Annotation'],
+        datasets: [{
+            label: 'MT MS Spent',
+            data: [mt_2, mt_3, mt_4],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var meanTimeLowInternet = new Chart(ctx2, {
+    type: 'bar',
+    data: {
+        labels: ['No AH', 'AH Hiding', 'AH Hiding + Annotation'],
+        datasets: [{
+            label: 'MT MS Spent',
+            data: [mt_2, mt_3, mt_4],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var meanTimeLowNews = new Chart(ctx2, {
+    type: 'bar',
+    data: {
+        labels: ['No AH', 'AH Hiding', 'AH Hiding + Annotation'],
+        datasets: [{
+            label: 'MT MS Spent',
+            data: [mt_2, mt_3, mt_4],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
